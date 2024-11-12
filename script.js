2b2605fd8c98972cfadc2ba836105accae8ea6ae
Kristina Faye <hellokristinafaye@gmail.com>
2024-11-11 19:36:36 -0800

    start of API call

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello bb!");
+const apiKey = "2bd5559124f13def23addea6864b8f2c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
+
+}
+
+
+
+
+
+
+
+checkWeather();
